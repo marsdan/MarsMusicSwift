50bf2f383dec555e1d6b07481aff97651065f677
peng zhao <zhaopeng@net.com>
2019-11-21 18:06:18 +0800

    首页列表开发中

--- MarsMusic/MarsMusic/ViewController.swift
+++ MarsMusic/MarsMusic/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+  
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return 10
+      }
+      
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "MSMHomePageCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell.init(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        return cell!
+      }
+    
 
 }
 
