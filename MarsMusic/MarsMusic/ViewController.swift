776c85a7e313ee286c8aebb70be6d84870ddaae8
peng zhao <zhaopeng@net.com>
2019-11-21 17:09:01 +0800

    创建工程

---
+++ MarsMusic/MarsMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarsMusic
+//
+//  Created by peng zhao on 2019/11/21.
+//  Copyright © 2019 Mars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarsMusic/Pods/Target Support Files/Pods-MarsMusicUITests/Pods-MarsMusicUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MarsMusicUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MarsMusicUITests
+@end
